b823b7268a69a40ab5b05650bd76f25ed753c336
Nimesha <nimesha1996@gmail.com>
2020-06-28 15:06:43 +0530

    5 marks added if even, substracted if odd

--- even or odd.py
+++ even or odd.py
@@ -1,6 +1,16 @@
 n1 = int(input('enter your number:',))  
+even = True
 
+# Checking if the number is even or odd
 if n1%2 == 0:
-    print('Its even number.')
+    print('It\'s an even number.')
 else :
-    print('Its odd number.')
+    print('It\'s an odd number.')
+    even=False
+    
+
+# Adding another number if even
+if even:
+    print('New value',n1+5)
+else:
+    print('New value',n1-5)
